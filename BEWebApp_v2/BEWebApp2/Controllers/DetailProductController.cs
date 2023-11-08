e1d48430c7865e47740dc78556e77fd7fa94ae02
Admin <Admin@TAINGUYEN>
2023-11-08 23:06:07 +0700

    Add project files.

---
+++ BEWebApp_v2/BEWebApp2/Controllers/DetailProductController.cs
@@ -0,0 +1,27 @@
+﻿using BEWebApp2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BEWebApp2.Controllers
+{
+    public class DetailProductController : Controller
+    {
+        private StoreMgmtEntities db = new StoreMgmtEntities();
+
+        // GET: DetailProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DetailProduct()
+        {   
+            List<Product> listProduct=db.Products.ToList();
+            List<Color> colorList = db.Colors.ToList();
+            return View();
+        }
+    }
+}
